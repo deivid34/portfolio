acfdb5f9cbc07d382d582528f71206bb9e137ed8
deivid34 <deividmelega33@gmail.com>
2024-08-10 14:40:42 -0300

    Corrigir caminho do diretório do app no workflow

--- backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a15b344d64abc2cff2de8ff5baf98186cac2f0c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc27e5da27a2e314c3334358ab3bf12cc8e2e990")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
